0e67cb0dd889d9f7f727329d8fd6a32f8422ad4b
mahadeva-shindhe <mahadeva.shindhe@gds.ey.com>
2022-10-15 23:02:52 +0530

    Chunk Oriented Step - Creating first Item Processor

---
+++ src/main/java/com/shindhe/processor/FirstItemProcessor.java
@@ -0,0 +1,11 @@
+package com.shindhe.processor;
+
+import org.springframework.batch.item.ItemProcessor;
+
+public class FirstItemProcessor implements ItemProcessor<Integer, Long> {
+    @Override
+    public Long process(Integer item) throws Exception {
+        System.out.println("Inside Item Processor");
+        return Long.valueOf(item + 20);
+    }
+}
